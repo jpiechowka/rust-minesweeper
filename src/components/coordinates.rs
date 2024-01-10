5db68076418ad2cb93a578b97d36c0600f62d04c
Janusz Piechówka <januszpiechowka@gmail.com>
2024-01-10 16:06:46 +0100

    chore: add impl Add<(i8, i8)> for Coordinates component

--- src/components/coordinates.rs
+++ src/components/coordinates.rs
@@ -26,6 +26,16 @@ impl Add for Coordinates {
     }
 }
 
+impl Add<(i8, i8)> for Coordinates {
+    type Output = Self;
+
+    fn add(self, (x, y): (i8, i8)) -> Self::Output {
+        let x = (self.x as i16 + x as i16) as u16;
+        let y = (self.y as i16 + y as i16) as u16;
+        Self { x, y }
+    }
+}
+
 impl Sub for Coordinates {
     type Output = Self;
 
